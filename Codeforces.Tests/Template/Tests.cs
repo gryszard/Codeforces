05e36dd7ee84cc108de5b1458be675b5c2fa8e01
gryszard <rysiupysiu234@gmail.com>
2026-08-31 14:12:21 +0200

    Templates

---
+++ Codeforces.Tests/Template/Tests.cs
@@ -0,0 +1,21 @@
+﻿namespace Codeforces.Tests.Template;
+
+internal class Tests
+{
+    [Test, TestCaseSource(nameof(TestCases))]
+    public void RunTest(string input, string expectedOutput)
+    {
+        using var @in = new StringReader(input);
+        using var @out = new StringWriter();
+
+        var solution = new Codeforces.Template.Solution(@in, @out);
+        solution.Run();
+
+        Assert.That(@out.ToString(), Is.EqualTo(expectedOutput));
+    }
+
+    private static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData("", "\r\n");
+    }
+}
---
+++ Codeforces/Template/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace Codeforces.Template;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var solution = new Solution(Console.In, Console.Out);
+        solution.Run();
+    }
+}
+
+public class Solution
+{
+    private readonly TextReader _textReader;
+    private readonly TextWriter _textWriter;
+
+    public Solution()
+    {
+        _textReader = Console.In;
+        _textWriter = Console.Out;
+    }
+
+    public Solution(TextReader textReader, TextWriter textWriter)
+    {
+        _textReader = textReader;
+        _textWriter = textWriter;
+    }
+
+    public void Run()
+    {
+        string? nextLine = _textReader.ReadLine();
+
+        if (!int.TryParse(nextLine, out var _))
+        {
+            _textWriter.WriteLine("ERROR");
+            return;
+        }
+
+        // TODO: Implement the rest of the logic.
+    }
+}
