78b1897caf18c7aef03433c403a797266fa383d2
gryszard <rysiupysiu234@gmail.com>
2026-09-01 08:16:06 +0200

    1114A Got Any Grapes?
    https://codeforces.com/problemset/problem/1114/A

---
+++ Codeforces.Tests/1114/A_Got_Any_Grapes.cs
@@ -0,0 +1,73 @@
+﻿namespace Codeforces.Tests._1114;
+
+internal class A_Got_Any_Grapes
+{
+    [Test, TestCaseSource(nameof(TestCases))]
+    public void RunTest(string input, string expectedOutput)
+    {
+        using var @in = new StringReader(input);
+        using var @out = new StringWriter();
+
+        var solution = new Codeforces._1114.A_Got_Any_Grapes.Solution(@in, @out);
+        solution.Run();
+
+        Assert.That(@out.ToString(), Is.EqualTo(expectedOutput));
+    }
+
+    private static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData("""
+            1 6 2
+            4 3 3
+            
+            """, """
+            YES
+
+            """);
+
+        yield return new TestCaseData("""
+            5 1 1
+            4 3 2
+            
+            """, """
+            NO
+
+            """);
+
+        yield return new TestCaseData("""
+            100000 100000 100000
+            100000 100000 99999
+            
+            """, """
+            NO
+
+            """);
+
+        yield return new TestCaseData("""
+            100000 100000 99999
+            100000 99999 100000
+            
+            """, """
+            NO
+
+            """);
+
+        yield return new TestCaseData("""
+            100000 100000 99999
+            99999 100000 100000
+            
+            """, """
+            NO
+
+            """);
+
+        yield return new TestCaseData("""
+            100000 100000 100000
+            100000 100000 100000
+            
+            """, """
+            YES
+
+            """);
+    }
+}
---
+++ Codeforces/1114/A_Got_Any_Grapes/Solution.cs
@@ -0,0 +1,76 @@
+﻿namespace Codeforces._1114.A_Got_Any_Grapes;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var solution = new Solution(Console.In, Console.Out);
+        solution.Run();
+    }
+}
+
+public class Solution
+{
+    private readonly TextReader _textReader;
+    private readonly TextWriter _textWriter;
+
+    public Solution()
+    {
+        _textReader = Console.In;
+        _textWriter = Console.Out;
+    }
+
+    public Solution(TextReader textReader, TextWriter textWriter)
+    {
+        _textReader = textReader;
+        _textWriter = textWriter;
+    }
+
+    public void Run()
+    {
+        string? nextLine = _textReader.ReadLine();
+        ArgumentException.ThrowIfNullOrWhiteSpace(nextLine);
+
+        var tokens = nextLine.Split(' ');
+        if (tokens is null || tokens.Length != 3)
+        {
+            throw new ArgumentException("Incorrect input values. Expecting three integers: X Y Z.");
+        }
+
+        if (!int.TryParse(tokens[0], out int x) ||
+            !int.TryParse(tokens[1], out int y) ||
+            !int.TryParse(tokens[2], out int z))
+        {
+            throw new ArgumentException("Cannot convert input values to ints.");
+        }
+
+        nextLine = _textReader.ReadLine();
+        ArgumentException.ThrowIfNullOrWhiteSpace(nextLine);
+
+        tokens = nextLine.Split(' ');
+        if (tokens is null || tokens.Length != 3)
+        {
+            throw new ArgumentException("Incorrect input values. Expecting three integers: A B C.");
+        }
+
+        if (!int.TryParse(tokens[0], out int a) ||
+            !int.TryParse(tokens[1], out int b) ||
+            !int.TryParse(tokens[2], out int c))
+        {
+            throw new ArgumentException("Cannot convert input values to ints.");
+        }
+
+        var enoughGrapesForAndrew = a >= x;
+        var enoughGrapesForDmitry = a + b >= x + y;
+        var enoughGrapesForMichal = a + b + c >= x + y + z;
+
+        if (enoughGrapesForAndrew && enoughGrapesForDmitry && enoughGrapesForMichal)
+        {
+            _textWriter.WriteLine("YES");
+        }
+        else
+        {
+            _textWriter.WriteLine("NO");
+        }
+    }
+}
