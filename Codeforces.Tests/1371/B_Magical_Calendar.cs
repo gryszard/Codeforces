d45f0323404dfbc0d27771f0ade28cbf235405f6
gryszard <rysiupysiu234@gmail.com>
2026-09-01 10:42:57 +0200

    1371B Magical Calendar
    https://codeforces.com/problemset/problem/1371/B

---
+++ Codeforces.Tests/1371/B_Magical_Calendar.cs
@@ -0,0 +1,52 @@
+﻿namespace Codeforces.Tests._1371;
+
+internal class B_Magical_Calendar
+{
+    [Test, TestCaseSource(nameof(TestCases))]
+    public void RunTest(string input, string expectedOutput)
+    {
+        using var @in = new StringReader(input);
+        using var @out = new StringWriter();
+
+        var solution = new Codeforces._1371.B_Magical_Calendar.Solution(@in, @out);
+        solution.Run();
+
+        Assert.That(@out.ToString(), Is.EqualTo(expectedOutput));
+    }
+
+    private static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData("""
+            5
+            3 4
+            3 2
+            3 1
+            13 7
+            1010000 9999999
+            
+            """, """
+            4
+            3
+            1
+            28
+            510049495001
+            
+            """);
+
+        yield return new TestCaseData($"""
+            5
+            1 1
+            2 1
+            1 2
+            {(int)1e9} {(int)1e9}
+            {(int)1e9} {(int)(1e9 - 1)}
+            """, $"""
+            1
+            1
+            1
+            499999999500000001
+            499999999500000000
+            
+            """);
+    }
+}
---
+++ Codeforces/1371/B_Magical_Calendar/Solution.cs
@@ -0,0 +1,72 @@
+﻿namespace Codeforces._1371.B_Magical_Calendar;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var solution = new Solution(Console.In, Console.Out);
+        solution.Run();
+    }
+}
+
+public class Solution(TextReader textReader, TextWriter textWriter) : BaseSolution(textReader, textWriter)
+{
+    public void Run()
+    {
+        RunTests(SingleTest);
+    }
+
+    private void SingleTest()
+    {
+        (var n, var r) = FetchTokens(2);
+
+        long result;
+
+        if (n > r)
+        {
+            result = r * (r + 1) / 2;
+        }
+        else
+        {
+            result = n * (n - 1) / 2 + 1;
+        }
+
+        _textWriter.WriteLine(result);
+    }
+}
+
+public class BaseSolution(TextReader textReader, TextWriter textWriter)
+{
+    protected readonly TextReader _textReader = textReader;
+    protected readonly TextWriter _textWriter = textWriter;
+
+    protected void RunTests(Action singleTest)
+    {
+        string? nextLine = _textReader.ReadLine();
+
+        if (!int.TryParse(nextLine, out var testCases))
+        {
+            _textWriter.WriteLine("ERROR");
+            return;
+        }
+
+        for (int i = 0; i < testCases; i++)
+        {
+            singleTest();
+        }
+    }
+
+    protected (long n, long r) FetchTokens(int tokensExpected)
+    {
+        string? nextLine = _textReader.ReadLine();
+        ArgumentException.ThrowIfNullOrWhiteSpace(nextLine);
+
+        var tokens = nextLine.Split(' ');
+        if (tokens is null || tokens.Length != tokensExpected)
+        {
+            throw new ArgumentException($"Incorrect input tokens. Expecting {tokensExpected} tokens separated by single space.");
+        }
+
+        return (long.Parse(tokens[0]), long.Parse(tokens[1]));
+    }
+}
