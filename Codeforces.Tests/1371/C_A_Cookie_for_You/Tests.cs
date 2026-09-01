cea7253c9bdade4700388065aeb797731616c8c9
gryszard <rysiupysiu234@gmail.com>
2026-09-01 07:02:49 +0200

    1371C A Cookie for You
    https://codeforces.com/problemset/problem/1371/C

---
+++ Codeforces.Tests/1371/C_A_Cookie_for_You/Tests.cs
@@ -0,0 +1,57 @@
+﻿namespace Codeforces.Tests._1371.C_A_Cookie_for_You;
+
+internal class Tests
+{
+    [Test, TestCaseSource(nameof(TestCases))]
+    public void RunTest(string input, string expectedOutput)
+    {
+        using var @in = new StringReader(input);
+        using var @out = new StringWriter();
+
+        var solution = new Codeforces._1371.C_A_Cookie_for_You.Solution(@in, @out);
+        solution.Run();
+
+        Assert.That(@out.ToString(), Is.EqualTo(expectedOutput));
+    }
+
+    private static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData("""
+            6
+            2 2 1 2
+            0 100 0 1
+            12 13 25 1
+            27 83 14 25
+            0 0 1 0
+            1000000000000000000 1000000000000000000 1000000000000000000 1000000000000000000
+            """, """
+            Yes
+            No
+            No
+            Yes
+            No
+            Yes
+            
+            """);
+
+        yield return new TestCaseData("""
+            7
+            10 1 10 1
+            10 1 9 2
+            10 1 11 1
+            0 0 1 0
+            100 0 100 0
+            0 100 100 0
+            50 50 100 0
+            """, """
+            Yes
+            No
+            No
+            No
+            Yes
+            Yes
+            Yes
+            
+            """);
+    }
+}
---
+++ Codeforces/1371/C_A_Cookie_for_You/Solution.cs
@@ -0,0 +1,77 @@
+﻿namespace Codeforces._1371.C_A_Cookie_for_You;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var solution = new Solution(Console.In, Console.Out);
+        solution.Run();
+    }
+}
+
+public class Solution
+{
+    private readonly TextReader _textReader;
+    private readonly TextWriter _textWriter;
+
+    public Solution()
+    {
+        _textReader = Console.In;
+        _textWriter = Console.Out;
+    }
+
+    public Solution(TextReader textReader, TextWriter textWriter)
+    {
+        _textReader = textReader;
+        _textWriter = textWriter;
+    }
+
+    public void Run()
+    {
+        string? nextLine = _textReader.ReadLine();
+
+        if (!int.TryParse(nextLine, out var testCases))
+        {
+            _textWriter.WriteLine("ERROR");
+            return;
+        }
+
+        for (int i = 0; i < testCases; i++)
+        {
+            SingleTest();
+        }
+    }
+
+    private void SingleTest()
+    {
+        string? nextLine = _textReader.ReadLine();
+        ArgumentException.ThrowIfNullOrWhiteSpace(nextLine);
+
+        var inputValues = nextLine.Split(' ');
+
+        if (inputValues is null || inputValues.Length != 4)
+        {
+            throw new ArgumentException("Incorrect input values. Expecting three longs: A B N M.");
+        }
+
+        if (!long.TryParse(inputValues[0], out long a) ||
+            !long.TryParse(inputValues[1], out long b) ||
+            !long.TryParse(inputValues[2], out long n) ||
+            !long.TryParse(inputValues[3], out long m))
+        {
+            throw new ArgumentException("Cannot convert input values to longs.");
+        }
+
+        var enoughCookiesForGuests = a + b >= n + m;
+        var enoughCookiesForSecondType = Math.Min(a, b) >= m;
+
+        if (enoughCookiesForGuests && enoughCookiesForSecondType)
+        {
+            _textWriter.WriteLine("Yes");
+        }
+        else
+        {
+            _textWriter.WriteLine("No");
+        }
+    }
+}
