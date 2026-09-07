eefeffde22d82f3cf34f40d0195a1a9b51b3eba1
gryszard <rysiupysiu234@gmail.com>
2026-09-07 11:39:54 +0200

    1898B Milena and Admirer
    https://codeforces.com/problemset/problem/1898/B

---
+++ Codeforces.Tests/Tests/1898/B_Milena_and_Admirer.cs
@@ -0,0 +1,86 @@
+﻿namespace Codeforces.Tests._1898;
+
+internal class B_Milena_and_Admirer
+{
+    [TestCaseSource(nameof(LoadLocalCases))]
+    public void RunTest(string input, string expectedOutput)
+    {
+        using var @in = new StringReader(input);
+        using var @out = new StringWriter();
+
+        var solution = new Codeforces._1898.B_Milena_and_Admirer(@in, @out);
+        solution.RunSolution();
+
+        Assert.That(@out.ToString(), Is.EqualTo(expectedOutput));
+    }
+
+    private static IEnumerable<TestCaseData> LoadLocalCases()
+    {
+        yield return new TestCaseData("""
+            4
+            3
+            1 3 2
+            4
+            1 2 3 4
+            3
+            3 2 1
+            7
+            1 4 4 3 5 7 6
+            
+            """, """
+            1
+            0
+            3
+            9
+
+            """);
+
+        yield return new TestCaseData("""
+            7
+            1
+            10
+            2
+            10 20
+            2
+            10 10
+            2
+            9 4
+            3
+            1 100 3
+            3
+            1 100 5
+            4
+            1 1000 100 6
+            
+            """, """
+            0
+            0
+            0
+            2
+            33
+            19
+            215
+
+            """);
+
+        yield return new TestCaseData("""
+            1
+            10
+            1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1
+            
+            """, """
+            8999999991
+
+            """);
+
+        yield return new TestCaseData("""
+            1
+            3
+            2 1 2
+            
+            """, """
+            1
+
+            """);
+    }
+}
---
+++ Codeforces/Solutions/1898/B_Milena_and_Admirer.cs
@@ -0,0 +1,77 @@
+﻿using Codeforces.Utilities;
+
+namespace Codeforces._1898;
+
+public class B_Milena_and_Admirer(TextReader textReader, TextWriter textWriter) : BaseSolution(textReader, textWriter)
+{
+    public override void RunSolution()
+    {
+        RunTests(SingleTest);
+    }
+
+    private void SingleTest()
+    {
+        var n = FetchSingleInt();
+        var nextLine = _textReader.ReadLine();
+        ArgumentException.ThrowIfNullOrWhiteSpace(nextLine);
+
+        if (n == 1)
+        {
+            _textWriter.WriteLine(0);
+            return;
+        }
+
+        var arrayAsText = new ArrayAsText(nextLine);
+
+        var lastToken = arrayAsText.PopLastArrayElement();
+        long totalOperations = 0;
+
+        for (int i = n - 2; i >= 0; i--)
+        {
+            var currentToken = arrayAsText.PopLastArrayElement();
+            var newOperations = (currentToken - 1) / lastToken;
+            lastToken = currentToken;
+
+            if (newOperations == 0)
+            {
+                continue;
+            }
+
+            totalOperations += newOperations;
+
+            var smallestNewValue = currentToken / (newOperations + 1);
+            lastToken = smallestNewValue;
+        }
+
+        _textWriter.WriteLine(totalOperations);
+    }
+
+
+    private class ArrayAsText
+    {
+        private int _currentLength;
+        private readonly string _arrayAsText;
+
+        public ArrayAsText(string arrayAsText)
+        {
+            _arrayAsText = arrayAsText;
+            _currentLength = arrayAsText.Length;
+        }
+
+        internal int PopLastArrayElement()
+        {
+            var currentArray = _arrayAsText.AsSpan()[.._currentLength];
+            var indexOfLastSeparator = currentArray.LastIndexOf(' ');
+            var lastToken = currentArray[(indexOfLastSeparator + 1)..];
+
+            _currentLength = indexOfLastSeparator;
+
+            if (!int.TryParse(lastToken, out int value))
+            {
+                throw new InvalidOperationException("Cannot parse last array token");
+            }
+
+            return value;
+        }
+    }
+}
