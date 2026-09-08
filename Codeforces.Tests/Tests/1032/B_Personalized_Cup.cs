4fe5f112218513af1dc479040bba35493872c45e
gryszard <rysiupysiu234@gmail.com>
2026-09-08 11:53:01 +0200

    1032B Personalized Cup
    https://codeforces.com/problemset/problem/1032/B

---
+++ Codeforces.Tests/Tests/1032/B_Personalized_Cup.cs
@@ -0,0 +1,84 @@
+﻿namespace Codeforces.Tests._1032;
+
+internal class B_Personalized_Cup
+{
+    [TestCaseSource(nameof(LoadLocalCases))]
+    public void RunTest(string input, string expectedOutput)
+    {
+        using var @in = new StringReader(input);
+        using var @out = new StringWriter();
+
+        var solution = new Codeforces._1032.B_Personalized_Cup(@in, @out);
+        solution.RunSolution();
+
+        Assert.That(@out.ToString(), Is.EqualTo(expectedOutput));
+    }
+
+    private static IEnumerable<TestCaseData> LoadLocalCases()
+    {
+        yield return new TestCaseData("""
+            tourist
+            
+            """, """
+            1 7
+            tourist
+
+            """);
+
+        yield return new TestCaseData("""
+            MyNameIsLifeIAmForeverByYourSideMyNameIsLife
+            
+            """, """
+            3 15
+            MyNameIsLifeIAm
+            ForeverByYourSi
+            deMyNameIsLife*
+
+            """);
+
+        yield return new TestCaseData("""
+            abcdefghijklmnopqrst
+            
+            """, """
+            1 20
+            abcdefghijklmnopqrst
+
+            """);
+
+        yield return new TestCaseData("""
+            abcdefghijklmnopqrsta
+            
+            """, """
+            2 11
+            abcdefghijk
+            lmnopqrsta*
+
+            """);
+
+        yield return new TestCaseData("""
+            abcdefghijklmnopqrstabcdefghijklmnopqrstabcdefghijklmnopqrstabcdefghijklmnopqrsta
+            
+            """, """
+            5 17
+            abcdefghijklmnopq
+            rstabcdefghijklm*
+            nopqrstabcdefghi*
+            jklmnopqrstabcde*
+            fghijklmnopqrsta*
+
+            """);
+
+        yield return new TestCaseData("""
+            abcdefghijklmnopqrstabcdefghijklmnopqrstabcdefghijklmnopqrstabcdefghijklmnopqrstabcdefghijklmnopqrst
+            
+            """, """
+            5 20
+            abcdefghijklmnopqrst
+            abcdefghijklmnopqrst
+            abcdefghijklmnopqrst
+            abcdefghijklmnopqrst
+            abcdefghijklmnopqrst
+
+            """);
+    }
+}
---
+++ Codeforces/Solutions/1032/B_Personalized_Cup.cs
@@ -0,0 +1,42 @@
+﻿using Codeforces.Utilities;
+
+namespace Codeforces._1032;
+
+public class B_Personalized_Cup(TextReader textReader, TextWriter textWriter) : BaseSolution(textReader, textWriter)
+{
+    public override void RunSolution()
+    {
+        SingleTest();
+    }
+
+    private void SingleTest()
+    {
+        var s = _textReader.ReadLine();
+        ArgumentException.ThrowIfNullOrWhiteSpace(s);
+
+        var length = s.Length;
+        var rows = (length - 1) / 20 + 1;
+        var columns = (length - 1) / rows + 1;
+        var stars = rows * columns - length;
+        var noStars = rows - stars;
+
+        _textWriter.WriteLine($"{rows} {columns}");
+
+        for (int i = 0; i < noStars; i++)
+        {
+            var start = i * columns;
+            var rowLength = columns;
+
+            _textWriter.WriteLine(s.AsSpan().Slice(start, rowLength));
+        }
+
+        var offset = noStars * columns;
+        for (int i = 0; i < stars; i++)
+        {
+            var start = offset + i * columns - i;
+            var rowLength = columns - 1;
+
+            _textWriter.WriteLine($"{s.AsSpan().Slice(start, rowLength)}*");
+        }
+    }
+}
