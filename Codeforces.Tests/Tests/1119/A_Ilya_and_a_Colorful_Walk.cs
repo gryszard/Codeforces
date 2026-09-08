4ac53e0688b6860712d6075677f188ef4b1e2a00
gryszard <rysiupysiu234@gmail.com>
2026-09-08 10:23:04 +0200

    1119A Ilya and a Colorful Walk
    https://codeforces.com/problemset/problem/1119/A

---
+++ Codeforces.Tests/Tests/1119/A_Ilya_and_a_Colorful_Walk.cs
@@ -0,0 +1,55 @@
+﻿namespace Codeforces.Tests._1119;
+
+internal class A_Ilya_and_a_Colorful_Walk
+{
+    [TestCaseSource(nameof(LoadLocalCases))]
+    public void RunTest(string input, string expectedOutput)
+    {
+        using var @in = new StringReader(input);
+        using var @out = new StringWriter();
+
+        var solution = new Codeforces._1119.A_Ilya_and_a_Colorful_Walk(@in, @out);
+        solution.RunSolution();
+
+        Assert.That(@out.ToString(), Is.EqualTo(expectedOutput));
+    }
+
+    private static IEnumerable<TestCaseData> LoadLocalCases()
+    {
+        yield return new TestCaseData("""
+            5
+            1 2 3 2 3
+            
+            """, """
+            4
+
+            """);
+
+        yield return new TestCaseData("""
+            3
+            1 2 1
+            
+            """, """
+            1
+
+            """);
+
+        yield return new TestCaseData("""
+            7
+            1 1 3 1 1 1 1
+            
+            """, """
+            4
+
+            """);
+
+        yield return new TestCaseData("""
+            7
+            1 1 1 1 3 1 1
+            
+            """, """
+            4
+
+            """);
+    }
+}
---
+++ Codeforces/Solutions/1119/A_Ilya_and_a_Colorful_Walk.cs
@@ -0,0 +1,46 @@
+﻿using Codeforces.Utilities;
+
+namespace Codeforces._1119;
+
+public class A_Ilya_and_a_Colorful_Walk(TextReader textReader, TextWriter textWriter) : BaseSolution(textReader, textWriter)
+{
+    public override void RunSolution()
+    {
+        SingleTest();
+    }
+
+    private void SingleTest()
+    {
+        var n = FetchSingleInt();
+        var colors = FetchTokens(n);
+        var firstColor = colors.First();
+        var lastColor = colors.Last();
+
+        if (firstColor != lastColor)
+        {
+            _textWriter.WriteLine(n - 1);
+            return;
+        }
+
+        int maximumDistance = 0;
+        for (int i = n - 2; i > 0; i--)
+        {
+            if (firstColor != colors[i])
+            {
+                maximumDistance = i;
+                break;
+            }
+        }
+
+        for (int i = 1; i < n - 1; i++)
+        {
+            if (colors[i] != lastColor)
+            {
+                maximumDistance = Math.Max(maximumDistance, n - 1 - i);
+                break;
+            }
+        }
+
+        _textWriter.WriteLine(maximumDistance);
+    }
+}
