43f888ed3dd2e7306e0e0f54e556a40e8beb828f
gryszard <rysiupysiu234@gmail.com>
2026-09-08 11:12:58 +0200

    393A Nineteen
    https://codeforces.com/problemset/problem/393/A

---
+++ Codeforces.Tests/Tests/0393/A_Nineteen.cs
@@ -0,0 +1,139 @@
+﻿namespace Codeforces.Tests._0393;
+
+internal class A_Nineteen
+{
+    [TestCaseSource(nameof(LoadLocalCases))]
+    public void RunTest(string input, string expectedOutput)
+    {
+        using var @in = new StringReader(input);
+        using var @out = new StringWriter();
+
+        var solution = new Codeforces._0393.A_Nineteen(@in, @out);
+        solution.RunSolution();
+
+        Assert.That(@out.ToString(), Is.EqualTo(expectedOutput));
+    }
+
+    private static IEnumerable<TestCaseData> LoadLocalCases()
+    {
+        yield return new TestCaseData("""
+            nniinneetteeeenn
+            
+            """, """
+            2
+
+            """);
+
+        yield return new TestCaseData("""
+            nneteenabcnneteenabcnneteenabcnneteenabcnneteenabcii
+            
+            """, """
+            2
+
+            """);
+
+        yield return new TestCaseData("""
+            nineteenineteen
+            
+            """, """
+            2
+
+            """);
+
+        yield return new TestCaseData("""
+            nneteennneteen
+            
+            """, """
+            0
+
+            """);
+
+        yield return new TestCaseData("""
+            nineteennneteen
+            
+            """, """
+            1
+
+            """);
+
+        yield return new TestCaseData("""
+            nineieennneieen
+            
+            """, """
+            0
+
+            """);
+
+        yield return new TestCaseData("""
+            nineieennneteen
+            
+            """, """
+            1
+
+            """);
+
+        yield return new TestCaseData("""
+            ninetennintnnintnint
+            
+            """, """
+            0
+
+            """);
+
+        yield return new TestCaseData("""
+            ninetennintnnintninet
+            
+            """, """
+            1
+
+            """);
+
+        yield return new TestCaseData("""
+            nneeeitieteeieteeietee
+            
+            """, """
+            0
+
+            """);
+
+        yield return new TestCaseData("""
+            nnneeeitieteeieteeietee
+            
+            """, """
+            1
+
+            """);
+
+        yield return new TestCaseData("""
+            nnnneeeitieteeieteeietee
+            
+            """, """
+            1
+
+            """);
+
+        yield return new TestCaseData("""
+            nnnnneeeitieteeieteeietee
+            
+            """, """
+            2
+
+            """);
+
+        yield return new TestCaseData("""
+            nnnnnneeeitieteeieteeietee
+            
+            """, """
+            2
+
+            """);
+
+        yield return new TestCaseData("""
+            nnnnnnneeeitieteeieteeietee
+            
+            """, """
+            3
+
+            """);
+    }
+}
---
+++ Codeforces/Solutions/0393/A_Nineteen.cs
@@ -0,0 +1,41 @@
+﻿using Codeforces.Utilities;
+
+namespace Codeforces._0393;
+
+public class A_Nineteen(TextReader textReader, TextWriter textWriter) : BaseSolution(textReader, textWriter)
+{
+    public override void RunSolution()
+    {
+        SingleTest();
+    }
+
+    private void SingleTest()
+    {
+        var s = _textReader.ReadLine();
+        ArgumentException.ThrowIfNullOrWhiteSpace(s);
+
+        char[] nineteenChars = ['n', 'i', 'e', 't'];
+        var occurrences = new Dictionary<char, int>
+        {
+            { 'n', 0 },
+            { 'i', 0 },
+            { 'e', 0 },
+            { 't', 0 }
+        };
+
+        foreach (var c in s)
+        {
+            if (nineteenChars.Contains(c))
+            {
+                occurrences[c]++;
+            }
+        }
+
+        var maximumWords = occurrences['t'];
+        maximumWords = Math.Min(maximumWords, occurrences['i']);
+        maximumWords = Math.Min(maximumWords, occurrences['e'] / 3);
+        maximumWords = Math.Min(maximumWords, (occurrences['n'] - 1) / 2);
+
+        _textWriter.WriteLine(maximumWords);
+    }
+}
