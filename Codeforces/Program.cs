5c607895195d90ace14837e20d20d654cc077bc0
gryszard <rysiupysiu234@gmail.com>
2026-09-15 14:24:15 +0200

    Add benchmark

--- Codeforces/Program.cs
+++ Codeforces/Program.cs
@@ -1,8 +1,12 @@
 ﻿using Codeforces.Utilities;
 using Codeforces._0123;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
 var solution = new B_Squares(Console.In, Console.Out);
 solution.RunSolution();
 
 //static BaseSolution solutionFactory(TextReader reader, TextWriter writer) => new Solution(reader, writer);
 //TestsCreator.PerformFullTests(solutionFactory);
+
+//BenchmarkRunner.Run<Benchmark>();
---
+++ Codeforces/Utilities/Benchmark.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Attributes;
+using Codeforces._0123;
+
+namespace Codeforces.Utilities;
+
+[MemoryDiagnoser]
+public class Benchmark
+{
+    [Benchmark]
+    public void Method()
+    {
+        using var @in = new StringReader("999999999 999999998 999999997 999999996 999999995 999999994");
+        using var @out = new StringWriter();
+
+        var solution = new B_Squares(@in, @out);
+        solution.RunSolution();
+    }
+}
