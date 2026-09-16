b9f966e5b1ac918ac1191abd39f608ea0f73c7b5
gryszard <rysiupysiu234@gmail.com>
2026-09-16 10:36:35 +0200

    2259E Treasure Map Destruction (Constructive Version)
    https://codeforces.com/problemset/problem/2259/E

---
+++ Codeforces.Tests/Tests/2259/E_Treasure_Map_Destruction.cs
@@ -0,0 +1,115 @@
+﻿namespace Codeforces.Tests._2259;
+
+internal class E_Treasure_Map_Destruction
+{
+    [TestCaseSource(nameof(LoadLocalCases))]
+    public void RunTest(string input, string expectedOutput)
+    {
+        using var @in = new StringReader(input);
+        using var @out = new StringWriter();
+
+        var solution = new Codeforces._2259.E_Treasure_Map_Destruction(@in, @out);
+        solution.RunSolution();
+
+        Assert.That(@out.ToString(), Is.EqualTo(expectedOutput));
+    }
+
+    private static IEnumerable<TestCaseData> LoadLocalCases()
+    {
+        yield return new TestCaseData("""
+            12
+            5
+            0 1 -1 -1 0
+            3
+            -1 0 2
+            5
+            -1 1 -1 1 -1
+            5
+            -1 -1 -1 -1 -1
+            5
+            -1 2 -1 3 -1
+            7
+            2 1 0 1 0 1 2
+            1
+            -1
+            3
+            1 -1 1
+            1
+            0
+            4
+            3 -1 -1 -1
+            6
+            -1 -1 0 -1 2 4
+            10
+            -1 1 -1 -1 1 1 -1 -1 2 -1
+            
+            """, """
+            10101
+            -1
+            10101
+            10000
+            -1
+            0010100
+            1
+            010
+            1
+            0001
+            -1
+            1011001000
+
+            """);
+
+        yield return new TestCaseData("""
+            2
+            3
+            -1 1 1
+            3
+            -1 -1 1
+            
+            """, """
+            -1
+            010
+
+            """);
+
+        yield return new TestCaseData("""
+            11
+            3
+            -1 -1 -1
+            3
+            1 -1 -1
+            3
+            1 1 0
+            3
+            1 1 -1
+            3
+            2 -1 1
+            3
+            -1 2 -1
+            3
+            1 -1 2
+            3
+            -1 -1 1
+            3
+            -1 1 1
+            5
+            3 -1 2 1 0
+            5
+            0 1 -1 2 3
+            
+            """, """
+            100
+            010
+            -1
+            -1
+            -1
+            -1
+            -1
+            010
+            -1
+            -1
+            -1
+
+            """);
+    }
+}
---
+++ Codeforces/Solutions/2259/E_Treasure_Map_Destruction.cs
@@ -0,0 +1,118 @@
+﻿using Codeforces.Utilities;
+
+namespace Codeforces._2259;
+
+public class E_Treasure_Map_Destruction(TextReader textReader, TextWriter textWriter) : BaseSolution(textReader, textWriter)
+{
+    public override void RunSolution()
+    {
+        RunTests(SingleTest);
+    }
+
+    private void SingleTest()
+    {
+        var n = FetchSingleInt();
+        var array = FetchTokens(n);
+
+        treasureMap = new int[n];
+        buffer = [(-1, -1), (-1, -1)];
+        bufferSize = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (array[i] == -1)
+            {
+                continue;
+            }
+
+            PutValue(i, array[i]);
+            if (!Analyze())
+            {
+                _textWriter.WriteLine(-1);
+                return;
+            }
+        }
+
+        if (bufferSize == 0)
+        {
+            treasureMap[0] = 1;
+        }
+
+        if (bufferSize > 0 && buffer[0].Index + buffer[0].Distance < n)
+        {
+            treasureMap[buffer[0].Index + buffer[0].Distance] = 1;
+        }
+
+        if (bufferSize > 0 && buffer[0].Index + buffer[0].Distance >= n && descending)
+        {
+            _textWriter.WriteLine(-1);
+            return;
+        }
+
+        _textWriter.WriteLine(string.Join(string.Empty, treasureMap));
+    }
+
+    private int[] treasureMap = [];
+    private (int Index, int Distance)[] buffer = [];
+    private int bufferSize = 0;
+    private bool descending = false;
+
+    private void PutValue(int index, int distance)
+    {
+        buffer[1] = buffer[0];
+        buffer[0] = (index, distance);
+
+        bufferSize = Math.Min(bufferSize + 1, 2);
+    }
+
+    private void PopValue()
+    {
+        bufferSize--;
+    }
+
+    private bool Analyze()
+    {
+        if (bufferSize == 1 && buffer[0].Index < buffer[0].Distance)
+        {
+            descending = true;
+            return true;
+        }
+
+        if (bufferSize == 1)
+        {
+            treasureMap[buffer[0].Index - buffer[0].Distance] = 1;
+            descending = false;
+            return true;
+        }
+
+        // bufferSize == 2
+        if (buffer[0].Index - buffer[1].Index < Math.Abs(buffer[0].Distance - buffer[1].Distance))
+        {
+            return false;
+        }
+
+        if (buffer[0].Index - buffer[1].Index >= Math.Abs(buffer[0].Distance + buffer[1].Distance))
+        {
+            treasureMap[buffer[0].Index - buffer[0].Distance] = 1;
+            treasureMap[buffer[1].Index + buffer[1].Distance] = 1;
+            descending = false;
+            PopValue();
+            return true;
+        }
+
+        if (descending && buffer[0].Distance > buffer[1].Distance - (buffer[0].Index - buffer[1].Index))
+        {
+            return false;
+        }
+
+        if (!descending && buffer[0].Distance < buffer[1].Distance + (buffer[0].Index - buffer[1].Index))
+        {
+            descending = true;
+            PopValue();
+            return true;
+        }
+
+        PopValue();
+        return true;
+    }
+}
