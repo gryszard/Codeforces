16e69713c54fe5f875b404bb0da5e78603019027
gryszard <rysiupysiu234@gmail.com>
2026-09-16 12:55:09 +0200

    2260C Maximize XOR, Minimize Operations
    https://codeforces.com/problemset/problem/2260/C

---
+++ Codeforces.Tests/Tests/2260/C_Maximize_XOR_Minimize_Operations.cs
@@ -0,0 +1,23 @@
+﻿using Codeforces.Tests.Utilities;
+
+namespace Codeforces.Tests._2260;
+
+internal class C_Maximize_XOR_Minimize_Operations
+{
+    [TestCaseSource(nameof(LoadExternalCases))]
+    public void RunTest(string input, string expectedOutput)
+    {
+        using var @in = new StringReader(input);
+        using var @out = new StringWriter();
+
+        var solution = new Codeforces._2260.C_Maximize_XOR_Minimize_Operations(@in, @out);
+        solution.RunSolution();
+
+        Assert.That(@out.ToString(), Is.EqualTo(expectedOutput));
+    }
+
+    private static IEnumerable<TestCaseData> LoadExternalCases()
+    {
+        return Utils.LoadExternalTestCases("2260", "C_Maximize_XOR_Minimize_Operations");
+    }
+}
---
+++ Codeforces/Solutions/2260/C_Maximize_XOR_Minimize_Operations.cs
@@ -0,0 +1,57 @@
+﻿using Codeforces.Utilities;
+
+namespace Codeforces._2260;
+
+public class C_Maximize_XOR_Minimize_Operations(TextReader textReader, TextWriter textWriter) : BaseSolution(textReader, textWriter)
+{
+    public override void RunSolution()
+    {
+        RunTests(SingleTest);
+    }
+
+    private void SingleTest()
+    {
+        var span = _textReader.ReadLine().AsSpan();
+        List<long> tokens = [];
+
+        foreach (var range in span.Split(' '))
+        {
+            var start = range.Start.Value;
+            int length = range.End.Value - start;
+            tokens.Add(long.Parse(span.Slice(start, length)));
+        }
+
+        var (x, y) = (tokens[0], tokens[1]);
+        long maxPossible = x + y;
+        long currentPower = 1;
+
+        while (currentPower < maxPossible)
+        {
+            currentPower *= 2;
+        }
+
+        long max = maxPossible;
+
+        while (currentPower > 0)
+        {
+            long xPart = x / currentPower;
+            long yPart = y / currentPower;
+            long maxPart = max / currentPower;
+
+            x %= currentPower;
+            y %= currentPower;
+            max %= currentPower;
+
+            if ((xPart ^ yPart) != maxPart)
+            {
+                _textWriter.WriteLine($"{maxPossible} {currentPower - y}");
+                return;
+            }
+
+            currentPower /= 2;
+        }
+
+        _textWriter.WriteLine($"{maxPossible} 0");
+        return;
+    }
+}
