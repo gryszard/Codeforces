ade034a700e311bd1037f4780eda59e544adefe8
gryszard <rysiupysiu234@gmail.com>
2026-09-17 10:21:13 +0200

    2256B Domino Tiles
    https://codeforces.com/problemset/problem/2256/B

---
+++ Codeforces.Tests/Tests/2256/B_Domino_Tiles.cs
@@ -0,0 +1,78 @@
+﻿namespace Codeforces.Tests._2256;
+
+internal class B_Domino_Tiles
+{
+    [TestCaseSource(nameof(LoadLocalCases))]
+    public void RunTest(string input, string expectedOutput)
+    {
+        using var @in = new StringReader(input);
+        using var @out = new StringWriter();
+
+        var solution = new Codeforces._2256.B_Domino_Tiles(@in, @out);
+        solution.RunSolution();
+
+        Assert.That(@out.ToString(), Is.EqualTo(expectedOutput));
+    }
+
+    private static IEnumerable<TestCaseData> LoadLocalCases()
+    {
+        yield return new TestCaseData("""
+            4
+            2
+            ??
+            5
+            0?1??
+            5
+            0?0??
+            8
+            00110011
+            
+            """, """
+            4
+            2
+            0
+            1
+
+            """);
+
+        yield return new TestCaseData("""
+            11
+            2
+            ??
+            2
+            ?1
+            2
+            00
+            3
+            ???
+            4
+            ????
+            8
+            0???????
+            8
+            1???????
+            8
+            01??????
+            8
+            01100110
+            8
+            0?0?????
+            8
+            0???1???
+            
+            """, """
+            4
+            2
+            1
+            4
+            4
+            2
+            2
+            1
+            1
+            0
+            0
+
+            """);
+    }
+}
---
+++ Codeforces/Solutions/2256/B_Domino_Tiles.cs
@@ -0,0 +1,73 @@
+﻿using Codeforces.Utilities;
+
+namespace Codeforces._2256;
+
+public class B_Domino_Tiles(TextReader textReader, TextWriter textWriter) : BaseSolution(textReader, textWriter)
+{
+    public override void RunSolution()
+    {
+        RunTests(SingleTest);
+    }
+
+    private void SingleTest()
+    {
+        var n = FetchSingleInt();
+        var s = FetchLine();
+
+        List<(bool ContainsZero, bool ContainsOne, bool ContainsQuestion)> groups = [new(), new(), new(), new()];
+        int currentModulo = -1;
+
+        foreach (var c in s)
+        {
+            currentModulo = (currentModulo + 1) % 4;
+            var (ContainsZero, ContainsOne, ContainsQuestion) = groups[currentModulo];
+
+            groups[currentModulo] = c switch
+            {
+                '0' => (true, ContainsOne, ContainsQuestion),
+                '1' => (ContainsZero, true, ContainsQuestion),
+                _ => (ContainsZero, ContainsOne, true)
+            };
+        }
+
+        foreach (var (ContainsZero, ContainsOne, _) in groups)
+        {
+            if (ContainsZero && ContainsOne)
+            {
+                _textWriter.WriteLine(0);
+                return;
+            }
+        }
+
+        int possibles = 1;
+
+        for (int i = 0; i < 2; i++)
+        {
+            if (groups[i].ContainsZero && groups[i].ContainsOne ||
+                groups[i + 2].ContainsZero && groups[i + 2].ContainsOne)
+            {
+                _textWriter.WriteLine(0);
+                return;
+            }
+
+            if (groups[i].ContainsZero && groups[i + 2].ContainsZero)
+            {
+                _textWriter.WriteLine(0);
+                return;
+            }
+
+            if (groups[i].ContainsOne && groups[i + 2].ContainsOne)
+            {
+                _textWriter.WriteLine(0);
+                return;
+            }
+
+            if (!groups[i].ContainsZero && !groups[i].ContainsOne && !groups[i + 2].ContainsZero && !groups[i + 2].ContainsOne)
+            {
+                possibles *= 2;
+            }
+        }
+
+        _textWriter.WriteLine(possibles);
+    }
+}
